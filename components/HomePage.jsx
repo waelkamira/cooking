501d68b5d58e942961baa1a6d9aff19927a3f527
waelkamira <ramondm4@gmail.com>
2024-06-23 23:13:31 +0300

    rebuild

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -8,6 +8,7 @@ import { useSession } from 'next-auth/react';
 import SideBarMenu from './SideBarMenu';
 import { TfiMenuAlt } from 'react-icons/tfi';
 import CategoriesSlides from './CategoriesSlides';
+import { Suspense } from 'react';
 
 export default function HomePage() {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,39 +18,41 @@ export default function HomePage() {
   }
 
   return (
-    <div className="relative flex flex-col justify-center items-center xl:w-4/5 z-40 sm:my-0 w-full bg-four">
-      <div className="w-full p-2 sm:p-4 lg:p-8">
-        <div className="xl:hidden absolute flex flex-col items-start gap-2 z-50 top-2 right-2 sm:top-4 sm:right-4 lg:top-12 lg:right-12 w-full">
-          <TfiMenuAlt
-            className=" p-2 rounded-lg text-4xl sm:text-5xl text-one animate-pulse"
-            onClick={() => setIsOpen(!isOpen)}
-          />
-          {isOpen && <SideBarMenu setIsOpen={setIsOpen} />}
-        </div>
-        <div className="relative hidden lg:block w-full h-24 sm:h-[200px] rounded-lg overflow-hidden shadow-lg shadow-one">
-          <Image
-            priority
-            src={
-              'https://res.cloudinary.com/dh2xlutfu/image/upload/v1718716946/cooking/1_trrjyx.png'
-            }
-            layout="fill"
-            objectFit="cover"
-            alt="photo"
-          />
-        </div>
-        <SearchBar />
+    <Suspense>
+      <div className="relative flex flex-col justify-center items-center xl:w-4/5 z-40 sm:my-0 w-full bg-four">
+        <div className="w-full p-2 sm:p-4 lg:p-8">
+          <div className="xl:hidden absolute flex flex-col items-start gap-2 z-50 top-2 right-2 sm:top-4 sm:right-4 lg:top-12 lg:right-12 w-full">
+            <TfiMenuAlt
+              className=" p-2 rounded-lg text-4xl sm:text-5xl text-one animate-pulse"
+              onClick={() => setIsOpen(!isOpen)}
+            />
+            {isOpen && <SideBarMenu setIsOpen={setIsOpen} />}
+          </div>
+          <div className="relative hidden lg:block w-full h-24 sm:h-[200px] rounded-lg overflow-hidden shadow-lg shadow-one">
+            <Image
+              priority
+              src={
+                'https://res.cloudinary.com/dh2xlutfu/image/upload/v1718716946/cooking/1_trrjyx.png'
+              }
+              layout="fill"
+              objectFit="cover"
+              alt="photo"
+            />
+          </div>
+          <SearchBar />
 
-        <h1 className="hidden xl:block text-md sm:text-lg lg:text-3xl text-nowrap mx-2 font-bold text-white bg-four rounded-full py-2 px-4 select-none text-center">
-          أحدث المنشورات
-        </h1>
+          <h1 className="hidden xl:block text-md sm:text-lg lg:text-3xl text-nowrap mx-2 font-bold text-white bg-four rounded-full py-2 px-4 select-none text-center">
+            أحدث المنشورات
+          </h1>
+        </div>
+        <div className="flex flex-col justify-center items-center w-full rounded-lg sm:p-8 gap-4 bg-[url(https://res.cloudinary.com/dh2xlutfu/image/upload/v1719060133/shoed13tnpenwajfs8sa.png)]">
+          <CategoriesSlides />
+          <h1 className="xl:hidden text-md sm:text-lg lg:text-3xl text-nowrap mx-2 font-bold text-white bg-four rounded-full py-2 px-4 select-none text-center">
+            أحدث المنشورات
+          </h1>
+          <AllCookingRecipes />
+        </div>
       </div>
-      <div className="flex flex-col justify-center items-center w-full rounded-lg sm:p-8 gap-4 bg-[url(https://res.cloudinary.com/dh2xlutfu/image/upload/v1719060133/shoed13tnpenwajfs8sa.png)]">
-        <CategoriesSlides />
-        <h1 className="xl:hidden text-md sm:text-lg lg:text-3xl text-nowrap mx-2 font-bold text-white bg-four rounded-full py-2 px-4 select-none text-center">
-          أحدث المنشورات
-        </h1>
-        <AllCookingRecipes />
-      </div>
-    </div>
+    </Suspense>
   );
 }
