dda7da186529ccb61358672e3d68a6ed6480c5ab
waelkamira <ramondm4@gmail.com>
2024-06-28 11:16:29 +0300

    improve

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    unoptimized: true,
-  },
+  distDir: 'build',
+
   images: {
     remotePatterns: [
       {
