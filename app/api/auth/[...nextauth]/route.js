620f5ed688eae092e0183dde5563c284d306fad4
waelkamira <127122203+waelkamira@users.noreply.github.com>
2024-07-20 10:58:30 +0300

    Delete app directory

--- app/api/auth/[...nextauth]/route.js
+++
@@ -1,4 +0,0 @@
-import NextAuth from 'next-auth/next';
-import { authOptions } from '../../authOptions/route';
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST };
--- app/api/createMeal/route.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-import { Meal } from '../models/CreateMealModel';
-export async function POST(req) {
-  await mongoose.connect(process.env.NEXT_PUBLIC_MONGODB);
-  const data = await req.json();
-  // console.log('data', data);
-  const meal = await Meal.create({ ...data });
-  return Response.json(meal);
-}
